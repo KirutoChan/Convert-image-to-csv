51a7130ce180b7d16d778801dbf0d9edc4c346e1
KirutoChan <kkristinaityte@gmail.com>
2017-05-02 11:25:02 +0300

    optimise img_to_csv

--- img_to_csv.py
+++ img_to_csv.py
@@ -1,17 +1,18 @@
 from PIL import Image
 import csv
 
-im = Image.open("scan41.png")
+file_name = 'met 2 scan 45 wf'
+im = Image.open(file_name + '.png')
 im = im.crop((420, 512, 1615, 1019))  			# to see only the spectrum
 im = im.transpose(Image.FLIP_TOP_BOTTOM)		# flipped verticaly because x=0 is in the top, not bottom
-im.save("scan41_croped.png")					# saves the croppped image
+#im.save(file_name + '_cropped' + '.png')		# saves the cropped image
 
 im.show()
 w = im.size[0]
 h = im.size[1]
 pix = im.load()
 
-with open("xy.csv", 'w', newline = '') as f:	# opens the empty csv file where we will put the spectrum values
+with open(file_name + ".csv", 'w', newline = '') as f:	# opens the empty csv file where we will put the spectrum values
 # check all pixels in image and if it is on our spectrum, we write x, y of that point to csv.
 	for i in range(6, w-1):						# from 6 because we need to exclude some points that we don't need
 		for j in range(0, h-5):					
